96d430574eac3a629c28ac070b94129a2d8351d6
Miao Chiang <miao@Miaos-MacBook-Pro.local>
2025-02-01 16:53:06 +0800

    Update Linux network settings and enable BBR

--- Linux/LinuxInit.sh
+++ Linux/LinuxInit.sh
@@ -74,7 +74,6 @@ net.ipv4.tcp_rmem = 4096 17500000 35000000
 net.ipv4.tcp_mtu_probing = 1
 net.ipv4.tcp_fastopen = 3
 net.ipv4.tcp_adv_win_scale = -2
-net.ipv4.tcp_collapse_max_bytes = 6291456
 net.ipv4.tcp_notsent_lowat = 131072
 EOF
 
