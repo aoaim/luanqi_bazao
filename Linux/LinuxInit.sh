69ee485eed10d0abfc45fa5d6bca223788acb306
Miao Chiang <miao@Miaos-MacBook-Pro.local>
2025-02-01 16:49:09 +0800

    Optimize Linux kernel settings and add new AI icons

--- Linux/LinuxInit.sh
+++ Linux/LinuxInit.sh
@@ -62,51 +62,30 @@ EOF
 
 # Sysctl
 cat > /etc/sysctl.conf <<EOF
-fs.file-max = 1048576
-fs.inotify.max_user_instances = 8192
-net.core.somaxconn = 32768
-net.core.netdev_max_backlog = 32768
-net.core.rmem_max = 33554432
-net.core.wmem_max = 33554432
-net.ipv4.udp_rmem_min = 16384
-net.ipv4.udp_wmem_min = 16384
-net.ipv4.tcp_rmem = 4096 87380 33554432
-net.ipv4.tcp_wmem = 4096 16384 33554432
-net.ipv4.tcp_mem = 786432 1048576 26777216
-net.ipv4.udp_mem = 65536 131072 262144
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_fin_timeout = 30
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.ip_local_port_range = 1024 65000
-net.ipv4.tcp_max_syn_backlog = 16384
-net.ipv4.tcp_max_tw_buckets = 6000
-net.ipv4.route.gc_timeout = 100
-net.ipv4.tcp_syn_retries = 1
-net.ipv4.tcp_synack_retries = 1
-net.ipv4.tcp_timestamps = 0
-net.ipv4.tcp_max_orphans = 131072
-net.ipv4.tcp_no_metrics_save = 1
-net.ipv4.tcp_ecn = 0
-net.ipv4.tcp_frto = 0
-net.ipv4.tcp_mtu_probing = 0
-net.ipv4.tcp_rfc1337 = 0
-net.ipv4.tcp_sack = 1
-net.ipv4.tcp_fack = 1
-net.ipv4.tcp_window_scaling = 1
-net.ipv4.tcp_adv_win_scale = 1
-net.ipv4.tcp_moderate_rcvbuf = 1
-net.ipv4.tcp_keepalive_time = 600
-net.ipv4.tcp_notsent_lowat = 16384
+fs.file-max = 1024000
+net.core.rmem_max = 35000000
+net.core.rmem_default = 17500000
+net.core.wmem_max = 35000000
+net.core.wmem_default = 17500000
+
+net.ipv4.tcp_wmem = 4096 17500000 35000000
+net.ipv4.tcp_rmem = 4096 17500000 35000000
+
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_adv_win_scale = -2
+net.ipv4.tcp_collapse_max_bytes = 6291456
+net.ipv4.tcp_notsent_lowat = 131072
 EOF
 
 # Enable BBR
 modprobe tcp_bbr &>/dev/null
 if grep -wq bbr /proc/sys/net/ipv4/tcp_available_congestion_control; then
-    echo "net.core.default_qdisc = fq" >>/etc/sysctl.conf
+    echo "net.core.default_qdisc = fq_pie" >>/etc/sysctl.conf
     echo "net.ipv4.tcp_congestion_control = bbr" >>/etc/sysctl.conf
 fi
 
 # Apply sysctl settings
-sysctl -p
+sysctl -p && sysctl --system
 
 echo "Successful kernel optimization"
