498e6822e4dcfcc772e39894be05e8ad1d12c7a9
aoaim <assertivemiao@outlook.com>
2025-10-31 22:53:48 +0800

    chore: update surge rule conversion to use DOMAIN instead of DOMAIN-SUFFIX

--- rule_generator/adobe_block_host_surge/src/services/surgeConverter.ts
+++ rule_generator/adobe_block_host_surge/src/services/surgeConverter.ts
@@ -1,3 +1,4 @@
 export function convertToSurgeRules(hosts: string[]): string[] {
-    return hosts.map(host => `DOMAIN-SUFFIX,${host}`);
+    // return hosts.map(host => `DOMAIN-SUFFIX,${host}`);
+    return hosts.map(host => `DOMAIN,${host}`);
 }
